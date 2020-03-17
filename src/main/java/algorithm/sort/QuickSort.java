44f9e7c0dacea525e5c29531fe19501fe1c6f1cf
lancelothe <whoami.ace@gmail.com>
2020-03-17 15:00:01 +0800

    auto update

---
+++ src/main/java/algorithm/sort/QuickSort.java
@@ -0,0 +1,100 @@
+package algorithm.sort;
+
+/**
+ * @author lancelot
+ * @date 2020/3/17
+ * 快速排序
+ */
+public class QuickSort {
+
+    /**
+     * 快速排序方法
+     *
+     * @param array
+     * @param start
+     * @param end
+     * @return
+     */
+    public static int[] quickSort(int[] array, int start, int end) {
+        if (array.length < 1 || start < 0 || end >= array.length || start > end) return null;
+        int smallIndex = partition(array, start, end);
+        if (smallIndex > start)
+            quickSort(array, start, smallIndex - 1);
+        if (smallIndex < end)
+            quickSort(array, smallIndex + 1, end);
+        return array;
+    }
+
+    /**
+     * 快速排序算法——partition
+     *
+     * @param array
+     * @param start
+     * @param end
+     * @return
+     */
+    public static int partition(int[] array, int start, int end) {
+        int pivot = (int) (start + Math.random() * (end - start + 1));
+        int smallIndex = start - 1;
+        swap(array, pivot, end);
+        for (int i = start; i <= end; i++)
+            if (array[i] <= array[end]) {
+                smallIndex++;
+                if (i > smallIndex)
+                    swap(array, i, smallIndex);
+            }
+        return smallIndex;
+    }
+
+    /**
+     * 交换数组内两个元素
+     *
+     * @param array
+     * @param i
+     * @param j
+     */
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+
+    //####################################################
+
+    public static void quickSort2(int[] arr, int low, int high) {
+        int i, j, temp, t;
+        if (low > high) {
+            return;
+        }
+        i = low;
+        j = high;
+        //temp就是基准位
+        temp = arr[low];
+
+        while (i < j) {
+            //先看右边，依次往左递减
+            while (temp <= arr[j] && i < j) {
+                j--;
+            }
+            //再看左边，依次往右递增
+            while (temp >= arr[i] && i < j) {
+                i++;
+            }
+            //如果满足条件则交换
+            if (i < j) {
+                t = arr[j];
+                arr[j] = arr[i];
+                arr[i] = t;
+            }
+
+        }
+        //最后将基准为与i和j相等位置的数字交换
+        arr[low] = arr[i];
+        arr[i] = temp;
+        //递归调用左半数组
+        quickSort2(arr, low, j - 1);
+        //递归调用右半数组
+        quickSort2(arr, j + 1, high);
+    }
+}
