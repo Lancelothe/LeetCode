069ce1b554523ec7539fb65580441aaa96cf2ae5
lancelothe <whoami.ace@gmail.com>
2020-07-25 15:45:02 +0800

    旋转数组

---
+++ src/main/java/algorithm/datastructure/array/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,48 @@
+package algorithm.datastructure.array;
+
+/**
+ * @author lancelot
+ * @date 2020/7/23
+ * @description leetcode [153] 寻找旋转排序数组中的最小值
+ * @difficulty Medium
+ * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+ * ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+ * 请找出其中最小的元素。
+ * 你可以假设数组中不存在重复元素。
+ *
+ * 示例 1:
+ * 输入: [3,4,5,1,2]
+ * 输出: 1
+ *
+ * 示例 2:
+ * 输入: [4,5,6,7,0,1,2]
+ * 输出: 0
+ *
+ * 链接：https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array
+ */
+public class FindMinimumInRotatedSortedArray {
+    /**
+     * 二分法
+     *
+     * @param nums
+     * @return
+     */
+    public int findMin(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new FindMinimumInRotatedSortedArray()
+                .findMin(new int[]{3, 4, 5, 6, 7, 1, 2}));
+    }
+}
